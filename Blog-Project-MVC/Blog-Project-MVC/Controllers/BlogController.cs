d039f561457782787963a8753517652d8ea98039
aleksey <alex.ki99@mail.ru>
2021-08-03 18:06:29 +0600

    Blog Initial

---
+++ Blog-Project-MVC/Blog-Project-MVC/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog_Project_MVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadBlog(Blog bg)
+        {
+
+
+            newdbEntities nd = new newdbEntities();
+            nd.Blogs.Add(bg);
+            nd.SaveChanges();
+            ViewBag.message = "Blog Details Are Saves Successfully...";
+            return View();
+        }
+    }
+}
